730bd923537a18570ab6cf4444a2f5b30017419a
Luciano Albuquerque <106563026+lucianofsociety@users.noreply.github.com>
2023-09-11 22:13:45 -0300

    inicializar
    Inicializando o versionamento do projeto de POO2

---
+++ projetoAula/src/br/ulbra/model/Usuario.java
@@ -0,0 +1,66 @@
+package br.ulbra.model;
+
+public class Usuario {
+
+    private int pkUsuario;
+    private String nomeUsu;
+    private String emailUsu;
+    private String dataNascUsu;
+    private int ativoUsu;
+    private String senhaUsu;
+
+    public int getPkUsuario() {
+        return pkUsuario;
+    }
+
+    public void setPkUsusario(int pkUsuario) {
+        this.pkUsuario = pkUsuario;
+    }
+
+    public String getNomeUsu() {
+        return nomeUsu;
+    }
+
+    public void setNomeUsu(String nomeUsu) {
+        this.nomeUsu = nomeUsu;
+    }
+
+    public String getEmailUsu() {
+        return emailUsu;
+    }
+
+    public void setEmailUsu(String emailUsu) {
+        this.emailUsu = emailUsu;
+    }
+
+    public String getSenhaUsu() {
+        return senhaUsu;
+    }
+
+    public void setSenhaUsu(String senhaUsu) {
+        this.senhaUsu = senhaUsu;
+    }
+
+    public String getDataNasc() {
+        return dataNascUsu;
+    }
+
+    public void setDataNasc(String dataNascUsu) {
+        this.dataNascUsu = dataNascUsu;
+    }
+
+    public int getAtivoUsu() {
+        return ativoUsu;
+    }
+
+    public void setAtivoUsu(int ativoUsu) {
+        this.ativoUsu = ativoUsu;
+    }
+
+    @Override
+    public String toString() {
+        return "USUARIO {" + "pkUsuario+" + pkUsuario + ",nomeUsu+" + nomeUsu
+                + ",emailUsu+" + emailUsu + ",dataNascUsu+" + dataNascUsu
+                + ",ativoUsu+" + ativoUsu + ",senhaUsu+" + senhaUsu + '}';
+    }
+}
---
+++ projetoAula/src/br/ulbra/model/UsuarioDAO.java
@@ -0,0 +1,84 @@
+package br.ulbra.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class UsuarioDAO {
+
+    private GerenciadorConexao gerenciador;
+
+    public UsuarioDAO() {
+        this.gerenciador = GerenciadorConexao.getInstancia();
+    }
+
+    public boolean autenticar(String email, String senha) {
+        String sql = "SELECT * from TBUSUARIO WHERE emailUsu = ? and senhaUsu = ? and ativoUsu =1";
+        try {
+            PreparedStatement stmt = gerenciador.getConexao().prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return false;
+    }
+
+    public void adicionarUsuario(String nome, String email, String senha, String datanasc, int ativo) {
+        String sql = "INSERT INTO TBUSUARIOS (nomeUsu, emailUsu, senhaUsu, datanascUsu, ativoUsu)"
+                + "VALUES (?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = gerenciador.getConexao().prepareStatement(sql);
+            stmt.setString(1, nome);
+            stmt.setString(2, email);
+            stmt.setString(3, senha);
+            stmt.setString(4, datanasc);
+            stmt.setInt(5, ativo);
+            JOptionPane.showMessageDialog(null, "Usuario: " + nome + "Inserido com sucesso! ");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro" + e.getMessage());
+        }
+    }
+
+    public List<Usuario> read() {
+        String sql = "SELECT * FROM tbusuario";
+        List<Usuario> usuarios = new ArrayList<>();
+
+        Connection con = gerenciador.getConexao();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = con.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                Usuario usuario = new Usuario();
+                usuario.setPkUsusario(rs.getInt("pkusu"));
+                usuario.setNomeUsu(rs.getString("nomeusu"));
+                usuario.setEmailUsu(rs.getString("emailusu"));
+                usuario.setSenhaUsu(rs.getString("senhausu"));
+                usuario.setDataNasc(rs.getString("datanascusu"));
+                usuario.setAtivoUsu(rs.getInt("ativousu"));
+
+                usuarios.add(usuario);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            GerenciadorConexao.closeConnection(con, stmt, rs);
+        }
+        return usuarios;
+    }
+
+}
